c256225fb57ba64bb53bab191188e4a2d62b883e
mateussc2000 <mateussc2000@gmail.com>
2026-03-08 20:25:07 -0300

    first commit

---
+++ src/main/java/com/encurtador_url/SuperApp/SuperAppApplication.java
@@ -0,0 +1,13 @@
+package com.encurtador_url.SuperApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/encurtador_url/SuperApp/SuperAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encurtador_url.SuperApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
